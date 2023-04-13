ca774285ff448f8219efd678630674176d8aceb1
Hiếu <86864023+hieudo062@users.noreply.github.com>
2023-04-13 11:14:14 +0700

    x

---
+++ producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -0,0 +1,26 @@
+package com.example.producer;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    @Bean
+    NewTopic notification() {
+        // topic name, partitionNumber, replicationNumber
+        return new NewTopic("vip", 2 , (short) 1);
+    }
+
+    @Bean
+    NewTopic statistic() {
+        // topic name, partitionNumber, replicationNumber
+        return new NewTopic("normal", 2 , (short) 1);
+    }
+}
